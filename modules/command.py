5a9f5d30c897c148048a39683f6b70f14b843d4d
yumehiko <46164249+yumehiko@users.noreply.github.com>
2023-03-27 01:29:43 +0900

    persona writer

--- modules/command.py
+++ modules/command.py
@@ -6,6 +6,7 @@ from modules import code_generator
 from modules import log_reader
 from modules import file_finder
 import re
+import json
 
 class Command(ABC):
     @abstractmethod
@@ -41,12 +42,12 @@ class ReadCommand(Command):
         self.chat_controller = chat_controller
 
     def match(self, message_text: str) -> bool:
-        return bool(re.match(r"^(Read:|read: ).*\.py$", message_text))
+        return bool(re.match(r"^(Read:|read: ).*\..*$", message_text))
 
     def execute(self, message: ChatMessage, system_talker: Talker) -> None:
-        command = message.text[6:]
-        file_name = command.split(".py")[0] + ".py"
-        source_code = file_finder.findSourceCode(file_name)
+        file_name = message.text[6:]
+        print(file_name)
+        source_code = file_finder.read_file(file_name)
         commandText = file_name + "の内容は次の通りです：\n" + source_code
         self.chat_controller.send_to_all(ChatMessage(commandText, system_talker.sender_info, False))
         message = ChatMessage("=== " + file_name + "のソースコードを読み上げました ===", system_talker.sender_info)
@@ -79,6 +80,7 @@ class EndCommand(Command):
         self.chat_controller.end_session()
         self.chat_controller.skip = True
     
+
 class GenerateModuleCommand(Command):
     def __init__(self, chat_controller: ChatController):
         self.chat_controller = chat_controller
@@ -90,3 +92,30 @@ class GenerateModuleCommand(Command):
         file_name = code_generator.write_py_file(message.text)
         message = ChatMessage("=== " + file_name + "を生成しました ===", system_talker.sender_info)
         self.chat_controller.print_message(message)
+
+
+class WritePersonaCommand(Command):
+    def __init__(self, chat_controller: ChatController):
+        self.chat_controller = chat_controller
+    
+    def match(self, message_text: str) -> bool:
+        return "execute: writePersona: " in message_text
+
+    def execute(self, message: ChatMessage, system_talker: Talker) -> None:
+        file_name = self.write_persona_file(message)
+        message = ChatMessage("Personaファイル: " + file_name + "が生成されました。", system_talker.sender_info)
+        self.chat_controller.print_message(message)
+
+    def write_persona_file(self, message: ChatMessage) -> str:
+        # execute: writePersona: を除く
+        persona_data = message.text
+        persona_data = persona_data.replace("execute: writePersona:", "").strip()
+        # 文字列をディクショナリに変換する
+        data = json.loads(persona_data)
+        # ファイル名を生成する
+        file_name = f"{data['name'].lower().replace(' ', '')}.json"
+        directory = "personas/"
+        # ディクショナリをJSONファイルに書き込む
+        with open(directory + file_name, "w") as json_file:
+                json.dump(data, json_file, ensure_ascii=False, indent=2)
+        return file_name
--- modules/file_finder.py
+++ modules/file_finder.py
@@ -1,29 +1,35 @@
 import os
 
-def findFilePath(file_name: str, search_path : str) -> str:
-  """
-  search_path以下のフォルダ内で、指定したファイル名を持つファイルのパスを返す。
-  該当するファイルがない場合は、空文字列を返す。
-  """
-  for root, dir, files in os.walk(search_path):
-      if file_name in files:
-          return os.path.join(root, file_name)
-  return ""
 
+def find_file_path(file_name: str) -> str:
+    """
+    リポジトリ内で、指定したファイル名を持つファイルのパスを返す。
+    該当するファイルがない場合は、空文字列を返す。
+    """
+    # モジュールファイルの親ディレクトリ
+    repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def findSourceCode(file_name: str) -> str:
-  """
-  指定したファイル名の中身を文字列として返す。
-  該当するファイルがない場合は、空文字列を返す。
-  """
+    print(repo_path)
 
-  search_path = os.path.dirname(os.path.abspath(__file__))
-  file_path = findFilePath(file_name, search_path)
-
-  # 該当するファイルがなかった場合は、空文字列を返す
-  if not file_path:
+    # リポジトリ直下とサブディレクトリをすべて探索する
+    for root, dirs, files in os.walk(repo_path):
+        if file_name in files:
+            return os.path.join(root, file_name)
     return ""
-  
-  # 該当するファイルがあった場合は、UTF-8エンコードでファイルを開いて中身を返す
-  with open(file_path, "r", encoding="utf-8") as file:
-    return file.read()
+
+
+def read_file(file_name: str) -> str:
+    """
+    指定したファイル名の中身を文字列として返す。
+    該当するファイルがない場合は、空文字列を返す。
+    """
+
+    file_path = find_file_path(file_name)
+
+    # 該当するファイルがなかった場合は、空文字列を返す
+    if not file_path:
+        return ""
+
+    # 該当するファイルがあった場合は、UTF-8エンコードでファイルを開いて中身を返す
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
